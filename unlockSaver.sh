db29a6e43c266a130cfbbcbedfd5e66b152b26dd
Andrew Johnson <Andrew.Johnson@stonybrook.edu>
2014-04-08 07:51:54 -0400

    Comments added.
    Commented the code, and that should be it. Once unleashed on the client
    computers it should enable administrators to unlock the console with
    admin credentials.

--- unlockSaver.sh
+++ unlockSaver.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
+	# Set up some variables: Location of file to be modded. Location of the 
+	# backupfile, and location of the temporary file.
 oldFile="/private/etc/pam.d/screensaver"
 bakFile="/private/etc/pam.d/screensaver.bak"
 tmpFile="/private/tmp/screensaver"
 
+	# If the backupfile doesn't exist, most likely the change hasn't been made.
+	# So backup the file just in case.
 if [ -e "$oldFile" ] && [ ! -e $bakFile ]; then
 	/bin/cp $oldFile $bakFile
 fi
 
+	# Now, check the file to see if it was actually modded. The change is made by 
+	# commenting out the last line which reads: 
+	# account    required       pam_group.so no_warn deny group=admin,wheel ruser fail_safe
 isMod=`/bin/cat $oldFile | /usr/bin/egrep -ic "#"`
 if [ $isMod -eq 2 ]; then
-	/bin/echo "File already changed."
+	/bin/echo "File has already been changed."
 else
 	/bin/echo "File needs to be changed."
-	/bin/cat $oldFile  | /usr/bin/sed -e "s/account    required       pam_group.so no_warn deny group=admin,wheel ruser fail_safe/# account    required       pam_group.so no_warn deny group=admin,wheel ruser fail_safe/" > $tmpFile
+		# Make the change with Sed. Basically add a # in front of the last line.
+		# Pipe the command to the temporary file.
+	/bin/cat $oldFile | /usr/bin/sed -e "s/account    required       pam_group.so no_warn deny group=admin,wheel ruser fail_safe/# account    required       pam_group.so no_warn deny group=admin,wheel ruser fail_safe/" > $tmpFile
+		# Copy the temporary file to replace the non modified file.
 	/bin/cp $tmpFile $oldFile
+		# Properly adjust ownership and permissions on the file, just in case.
 	/bin/chmod 644 $oldFile
 	/usr/sbin/chown root:wheel $oldFile
+		# Cleanup: Remove the temporary file.
 	/bin/rm $tmpFile
+		# Double check to see if the file was successfully changed.
 	isMod=`/bin/cat $oldFile | /usr/bin/egrep -ic "#"`
 	if [ $isMod -eq 2 ]; then
 		/bin/echo "File changed successfully."
 	else
 		/bin/echo "File didn't get changed."
+		exit 1
 	fi
 fi
 
 exit 0
-
